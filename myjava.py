71b5ddeea25b46cd5a98e4eb7aa8f940f6652489
tyrion <tyrion@hotmail.com>
2018-01-24 16:02:09 +0800

    Pass java code file name as the cmd parameter, but not the class name

--- myjava.py
+++ myjava.py
@@ -1,9 +1,13 @@
 import os
 import sys
 
-job = "javac %s" % sys.argv[1] + ".java"
+code_file = sys.argv[1]
+section = code_file.split(".")
+class_name = section[0]
+
+job = "javac %s" % code_file
 print(job)
 os.system(job)
-job = "java %s" % sys.argv[1]
+job = "java %s" % class_name
 print(job)
 os.system(job)
